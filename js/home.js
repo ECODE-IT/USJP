3f89ecc54267459a7a3dba5c0f597143f630145f
Rajiva Pavan <rajivapitiwaduge@gmail.com>
2021-09-15 10:30:31 +0530

    update

--- js/home.js
+++ js/home.js
@@ -2,3 +2,10 @@ $(".semester").click(function(){
     var semesterDetails = $(this).next();
     semesterDetails.collapse('toggle');
 });
+
+$("#search-courses").focus(function(){
+    $(this).parent().addClass('blue-glow');
+});
+$("#search-courses").blur(function(){
+    $(this).parent().removeClass('blue-glow');
+});
